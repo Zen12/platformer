479cb6fc222c8603182ce9d9d70c685db1e4db0a
Andrei Gusan <andrei1292@gmail.com>
2025-05-19 22:37:33 +0300

    fix apple

--- run_debug.sh
+++ run_debug.sh
@@ -1,3 +1,3 @@
-cmake -H. -Bbin/desktop -DPLATFORM=Desktop -DCMAKE_BUILD_TYPE=Debug &&
+cmake -H. -Bbin/debug/desktop -DPLATFORM=Desktop -DCMAKE_BUILD_TYPE=Debug &&
 cmake --build bin/debug/desktop/ &&
 bin/debug/desktop/Platformer
