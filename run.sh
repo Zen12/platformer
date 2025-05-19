92df6b3bc1fe3995cd7b06d3959107ea265fa6e1
Andrei Gusan <andrei1292@gmail.com>
2025-05-19 22:27:49 +0300

    Better build tools

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-cmake -H. -Bbin/desktop -DPLATFORM=Desktop &&
-cmake --build bin/desktop/ &&
-bin/desktop/Platformer
+cmake -H. -Bbin/release/desktop -DPLATFORM=Desktop -DCMAKE_BUILD_TYPE=Release &&
+cmake --build ./bin/release/desktop &&
+bin/release/desktop/Platformer
---
+++ run_debug.sh
@@ -0,0 +1,3 @@
+cmake -H. -Bbin/desktop -DPLATFORM=Desktop -DCMAKE_BUILD_TYPE=Debug &&
+cmake --build bin/debug/desktop/ &&
+bin/debug/desktop/Platformer
